c81e6e922812bcccf7a319cb78aaf2069dd16deb
abdumutalib1 <94834070+abdumutalib1@users.noreply.github.com>
2021-11-26 09:16:02 +0500

    C#
    C#

---
+++ HW_4.1/HW_4.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HW_4.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW_4.1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("13627afb-5d80-40c1-9f62-34a2518e9530")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HW_4.2/HW_4.2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4._2
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Месяц");
+            string grade = Console.ReadLine();
+            switch (grade)
+            {
+                case "январь":
+                    result = "30 руз";
+                    break;
+                case "февраль":
+                    result = "28 руз";
+                    break;
+                case "март":
+                    result = "30 руз";
+                    break;
+                case "апрел":
+                    result = "31 руз";
+                    break;
+                case "май":
+                    result = "30 руз";
+                    break;
+                case "июнь":
+                    result = "31 руз";
+                    break;
+                case "июль":
+                    result = "30 руз";
+                    break;
+                case "август":
+                    result = "31 руз";
+                    break;
+                case "сентябрь":
+                    result = "30 руз";
+                    break;
+                case "октябрь":
+                    result = "31 руз";
+                    break;
+                case "ноябрь":
+                    result = "30 руз";
+                    break;
+                case "декабрь":
+                    result = "31 руз";
+                    break;
+                default:
+                    result = "you failed!";
+                    break;
+            }
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
