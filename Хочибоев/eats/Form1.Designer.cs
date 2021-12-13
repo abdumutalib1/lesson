a801533f0e6630ba27164a433fa0c16c095753c1
abdumutalib1 <94834070+abdumutalib1@users.noreply.github.com>
2021-12-13 23:56:50 +0500

    Add files via upload

---
+++ Хочибоев/eats/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace eats
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu";
+            // 
+            // radioButton
+            // 
+            this.radioButton.AutoSize = true;
+            this.radioButton.Location = new System.Drawing.Point(38, 49);
+            this.radioButton.Name = "radioButton";
+            this.radioButton.Size = new System.Drawing.Size(79, 21);
+            this.radioButton.TabIndex = 1;
+            this.radioButton.TabStop = true;
+            this.radioButton.Text = "Chicken";
+            this.radioButton.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(127, 49);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 21);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Egg";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantity";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(30, 348);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 132);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Results";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(72, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(297, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Copy the previous reguest";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Submit new reguest";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(72, 243);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(297, 31);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Prepare Food";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Egg Quality:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 502);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Хочибоев/eats/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eats
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class ChickenOrder
+        {
+            int quantity;
+            public ChickenOrder(int comingquantity)
+            {
+                quantity = comingquantity;
+            }
+            public int GetQuantity()
+            {
+                return quantity;
+            }
+
+            public void CutUp()
+            {
+
+            }
+            public void cook()
+            {
+
+            }
+        }
+
+        public class EggOrder
+        {
+            Random rnd = new Random();
+            int quanity;
+            int quality;
+            public EggOrder(int comingquantity)
+            {
+                quanity = comingquantity;
+                quality = rnd.Next(101);
+
+            }
+            public Nullable<int> GetQuantity()
+            {
+                return quanity;
+            }
+            public int GetQuality()
+            {
+                return quality;
+            }
+            public void Crack()
+            {
+                if (quality < 25)
+                {
+                    throw new Exception("egg quality is <= 25");
+                }
+            }
+            public void DisShell()
+            {
+
+            }
+            public String cook()
+            {
+                string result = quanity + " eggs are cooking";
+                return result;
+            }
+        }
+        public class Employee
+        {
+            Object currentOrder;
+            public Employee()
+            {
+
+            }
+            public Object NewRequest(int quantity, int itemid)
+            {
+                if (itemid == 0)
+                {
+                    currentOrder = new ChickenOrder(quantity);
+                    return currentOrder;
+                }
+                else
+                {
+                    currentOrder = new EggOrder(quantity);
+                    return currentOrder;
+                }
+            }
+
+            public String Inspect()
+            {
+                if (currentOrder.GetType() == typeof(ChickenOrder))
+                {
+                    return "Chicken have not qulity!";
+                }
+                else
+                {
+                    return ((EggOrder)currentOrder).GetQuality().ToString();
+                }
+            }
+            public string PrepareFood()
+            {
+                if (currentOrder.GetType() == typeof(ChickenOrder))
+                {
+                    ChickenOrder ord = (ChickenOrder)currentOrder;
+                    for(int i = 1; i<= ord.GetQuantity(); i++)
+                    {
+                        ord.CutUp();
+                    }
+                    ord.cook();
+                    return ord.GetQuantity().ToString() + " chikkens is ready";
+                }
+                else
+                {
+                    EggOrder ord = (EggOrder)currentOrder;
+                    for (int i = 1; i <= ord.GetQuantity(); i++)
+                    {
+                        ord.Crack();
+                        ord.DisShell();
+                    }
+                    ord.cook();
+                    return ((EggOrder)currentOrder).GetQuantity().ToString() + " eggs is ready";
+                }
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Employee employee = new Employee();
+        int currentOrderId = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = int.Parse(textBox1.Text);
+            currentOrderId++;
+            if (currentOrderId % 2 == 0)
+            {
+                count = 0;
+            }
+            if (radioButton.Checked)
+            {
+                employee.NewRequest(count, 0);
+            }
+            if (radioButton1.Checked)
+            {
+                employee.NewRequest(count, 1);
+            }
+            string eggQuality = employee.Inspect();
+            label4.Text = eggQuality;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string result = employee.PrepareFood();
+            listBox1.Items.Add(result);
+        }     
+    }
+}
